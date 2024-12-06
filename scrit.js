dde657e7b27e948efdd2f8767e56c2cbcafc12af
OmarNino11 <omar.nino.parodi11@gmail.com>
2024-12-06 11:45:35 -0500

    commit js

---
+++ scrit.js
@@ -0,0 +1,4 @@
+document.getElementById('contacForm').addEventListener('submit',function(event){
+    event.preventDefault();//evitar que se recargue la pagina
+    alert('Formulario enviado correctamente gracias por contactarnos...!');
+});
